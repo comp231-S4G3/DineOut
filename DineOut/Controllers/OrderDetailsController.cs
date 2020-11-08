fc0ae90d55dd90077d914d3b463559ff346e782b
ederson marques <edmarques.ca2018@gmail.com>
2020-11-07 21:04:04 -0500

    orderdetails controller and view created

---
+++ DineOut/Controllers/OrderDetailsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DineOut.Models;
+
+namespace DineOut.Controllers
+{
+    public class OrderDetailsController:Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
