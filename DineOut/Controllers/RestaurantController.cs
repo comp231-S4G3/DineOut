37709f195db75fafca81e7788798169136806a37
Dioums <mdiallo8@my.centennialcollege.ca>
2020-11-08 00:04:26 -0500

    Added restaurant controller [issue #16], updated restaurant view and its components [issue #15], updated order list view [issue #25], created a partial view for restaurant, updated restaurant layout, created CRUD views for items

--- DineOut/Controllers/RestaurantController.cs
+++ DineOut/Controllers/RestaurantController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DineOut.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,35 @@ namespace DineOut.Controllers
 {
     public class RestaurantController : Controller
     {
+        private IRestaurantRepository repository;
 
+        public RestaurantController (IRestaurantRepository repo)
+        {
+            repository = repo;
+        }
+
+
+        //This belongs to Order Controller
+        //Added here, so we do not forget
+        /*public IActionResult CurrentOrders() 
+        {
+            return View();
+        }*/
+
+        //This belongs to Order Controller
+        //Added here, so we do not forget
+        /*public IActionResult CompletedOrders()
+        {
+            return View();
+        }*/
+
+        public IActionResult List(string menuID, string created)
+        {
+            return View(repository.Item
+                .Where(m => m.MenuId == menuID)
+                .OrderBy(i => i.CreatedOn == created));
+        }
+
+        //Create Profile Action bellow
     }
 }
