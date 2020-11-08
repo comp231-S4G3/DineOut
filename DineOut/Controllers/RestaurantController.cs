42dcbe7cca3ff16391df548c7d6daea2d2ca430c
ederson marques <edmarques.ca2018@gmail.com>
2020-11-08 16:51:48 -0500

    updating Ederson branch again

---
+++ DineOut/Controllers/RestaurantController.cs
@@ -0,0 +1,43 @@
+﻿using DineOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DineOut.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        DineOutContext DineOutContext = new DineOutContext();
+
+        public RestaurantController ()
+        {
+            
+        }
+
+
+        //This belongs to Order Controller
+        //Added here, so we do not forget
+        /*public IActionResult CurrentOrders() 
+        {
+            return View();
+        }*/
+
+        //This belongs to Order Controller
+        //Added here, so we do not forget
+        /*public IActionResult CompletedOrders()
+        {
+            return View();
+        }*/
+
+        public IActionResult Menu(int menuID, DateTime created)
+        {
+            return View(DineOutContext.Item
+                .Where(m => m.MenuId == menuID)
+                .OrderBy(i => i.CreatedOn == created));
+        }
+
+        //Create Profile Action bellow
+    }
+}
