b60eb983ee3ea343a7e3b9cb42b0f96861a80b2b
Tomoki Takeuchi <37243504+tomokitointhe6ix@users.noreply.github.com>
2020-11-14 09:15:05 -0500

    Created Infrastructure folder for #23

---
+++ DineOut/Infrastructure/SessionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace DineOut.Infrastructure
+{
+    public static class SessionExtensions
+    {
+        public static void SetJson(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        public static T GetJson<T>(this ISession session, string key)
+        {
+            var sessionData = session.GetString(key);
+
+            return sessionData == null
+                ? default(T)
+                : JsonConvert.DeserializeObject<T>(sessionData);
+        }
+
+    }
+}
