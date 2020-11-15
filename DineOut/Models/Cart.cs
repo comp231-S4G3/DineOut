677fbd43e2233e85e2f22d59dcc9f5072f50f023
Mansour M <latsous@gmail.com>
2020-11-15 12:58:44 -0500

    adding order summary and cartindex

---
+++ DineOut/Models/Cart.cs
@@ -0,0 +1,44 @@
+﻿using DineOut.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DineOut.Models
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+        public virtual void AddItem(Item item, int quantity)
+        {
+            CartLine line = lineCollection
+            .Where(i => i.Item.ItemId == item.ItemId)
+            .FirstOrDefault();
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine
+                {
+                    Item = item,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+        public virtual void RemoveLine(Item item) =>
+        lineCollection.RemoveAll(l => l.Item.ItemId == item.ItemId);
+        public virtual double ComputeTotalValue() =>
+        lineCollection.Sum(e => e.Item.Price * e.Quantity);
+        public virtual void Clear() => lineCollection.Clear();
+        public virtual IEnumerable<CartLine> Lines => lineCollection;
+    }
+    public class CartLine
+    {
+        public int CartLineID { get; set; }
+        public Item Item { get; set; }
+        public int Quantity { get; set; }
+    }
+}
+
---
+++ DineOut/ViewModels/CartIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using DineOut.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DineOut.ViewModels
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
+
