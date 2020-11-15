4f6a50871cde9fa841b138c50e34450f6d9f52e7
Tomoki Takeuchi <37243504+tomokitointhe6ix@users.noreply.github.com>
2020-11-15 00:53:52 -0500

    Added CustomerOrderViewModel for #22

--- DineOut/ViewModels/CustomerOrderViewModel.cs
+++ DineOut/ViewModels/CustomerOrderViewModel.cs
@@ -9,7 +9,15 @@ namespace DineOut.ViewModels
     public class CustomerOrderViewModel
     {
         public List<Item> Items { get; set; }
-        public Order Orders { get; set; }
-        public Restaurant Restaurants { get; set; }
+        public Item Item { get; set; }
+        public Order Order { get; set; }
+        public OrderItem OrderItem { get; set; }
+        public Restaurant Restaurant { get; set; }
+        public Menu Menu { get; set; }
+        public List<Customer> Customers { get; set; }
+        public List<CustomerOrder> CustomerOrders { get; set; }
+        public CustomerOrder CustomerOrder { get; set; }
+
+
     }
 }
