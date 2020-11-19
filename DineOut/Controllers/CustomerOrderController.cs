2df24f059aa3054bd2f8c891e0395ecf6840f77c
Tomoki Takeuchi <37243504+tomokitointhe6ix@users.noreply.github.com>
2020-11-19 18:41:49 -0500

    Updated

--- DineOut/Controllers/CustomerOrderController.cs
+++ DineOut/Controllers/CustomerOrderController.cs
@@ -12,7 +12,8 @@ namespace DineOut.Controllers
     public class CustomerOrderController : Controller
     {
         DineOutContext DineOutContext = new DineOutContext();
-        CustomerOrderViewModel CustomerOrderViewModel = new CustomerOrderViewModel();
+        CustomerOrderViewModel orderData = new CustomerOrderViewModel();
+        OrderDetailsInfo orderDetailsInfo = new OrderDetailsInfo();
 
         public IActionResult OrderDetails(int menuId, int customerId)
         {
@@ -25,151 +26,128 @@ namespace DineOut.Controllers
             nOrder.CustomerId = customerId; //newOrder.Customer.CustomerId
 
             DineOutContext.Order.Add(nOrder);
-            CustomerOrderViewModel.Order = DineOutContext.Order.Find(customerId);
-            CustomerOrderViewModel.Menu = DineOutContext.Menu.Find(menuId);
-            CustomerOrderViewModel.Items = DineOutContext.Item
+            //CustomerOrderViewModel.Order = DineOutContext.Order.Find(customerId);
+            orderData.Order
+                = DineOutContext.Order.Find(nOrder.OrderId);
+            orderData.Customer
+               = DineOutContext.Customer.Find(nOrder.CustomerId);
+            orderData.Menu = DineOutContext.Menu.Find(menuId);
+            orderData.Items = DineOutContext.Item
                 .ToList().FindAll(x => x.MenuId == menuId);
-            CustomerOrderViewModel.Restaurant = DineOutContext.Restaurant
-                .Find(CustomerOrderViewModel.Menu.RestaurantId);
-            foreach (Item item in CustomerOrderViewModel.Items)
+            orderData.Restaurant = DineOutContext.Restaurant
+                .Find(orderData.Menu.RestaurantId);
+            foreach (Item item in orderData.Items)
             {
-                CustomerOrderViewModel.Item = DineOutContext.Item.Find(item.ItemId);
+                orderData.Item = DineOutContext.Item.Find(item.ItemId);
             }
-            return View(CustomerOrderViewModel);
+            return View(orderData);
         }
 
         //Create a new order
         [HttpPost]
-        public IActionResult AddItem(CustomerOrderViewModel order)
+        public IActionResult AddItem(CustomerOrderViewModel order,
+            int quantity, int orderId, int customerId)
         {
-            //Demo customerId (Should be relpased with newOrder.Customer.CustomerId later)
-            int customerId = 1;
-            CustomerOrderViewModel orderData = new CustomerOrderViewModel();
-            OrderItem orderItem = new OrderItem();
-            //CustomerOrderViewModel customerOrderView= new CustomerOrderViewModel();
+            OrderItem orderItem = new OrderItem
+            {
+                OrderId = orderId,
+                ItemId = order.Item.ItemId
+            };
 
-            orderItem.ItemId = order.Item.ItemId;//set ItemId
-            if (order.OrderItem.OrderItemId != 0)
-                orderItem.OrderItemId = order.OrderItem.OrderItemId;//set OrderItemId
+            Order nOrder = new Order
+            {
+                OrderId = orderId,
+                CustomerId = customerId
+            };
+            DineOutContext.Order.Update(nOrder);
 
             if (ModelState.IsValid)
             {
-                Order nOrder = new Order();
-                nOrder.CustomerId = customerId; //newOrder.Customer.CustomerId
+                //To check if the item already exists in the order
+                CustomerOrderViewModel checkItem = new CustomerOrderViewModel();
 
-                if (order.Order.OrderId == 0)
+                //Check if the same orderId already exists
+                if (DineOutContext.Order_Item
+                    .Any(x => x.OrderId == orderId))
                 {
-                    DineOutContext.Order.Add(nOrder);//pass customerId
-                    orderItem.OrderId = nOrder.OrderId;
-                    orderItem.Quantity = order.OrderItem.Quantity;
-                    DineOutContext.Order_Item.Add(orderItem);
+                    checkItem.OrderItem = DineOutContext.Order_Item.Find(orderItem.OrderId);
+
+                    if (checkItem.OrderItem.ItemId == order.Item.ItemId)
+                    {
+                        checkItem.OrderItem.Quantity += quantity;
+                        DineOutContext.Order_Item.Update(checkItem.OrderItem);
+                        DineOutContext.SaveChanges();
+                    }
+                    else
+                    {
+                        orderItem.Quantity = quantity;
+                        DineOutContext.Order_Item.Add(orderItem);
+                    }
                 }
                 else
                 {
-                    orderItem.OrderId = nOrder.OrderId;
-                    if (order.OrderItem.Quantity == 0)
-                        orderItem.Quantity = order.OrderItem.Quantity;
-                    orderItem.Quantity += order.OrderItem.Quantity;
-                    DineOutContext.Order_Item.Update(orderItem);
+                    orderItem.Quantity = quantity;
+                    DineOutContext.Order_Item.Add(orderItem);
                 }
 
-                orderData.Order.OrderId = orderItem.OrderId;
-                orderData.Menu = DineOutContext.Menu
-                    .Find(order.Item.MenuId);
+                orderData.Order = DineOutContext.Order.Find(orderItem.OrderId);
+                orderData.Customer
+                    = DineOutContext.Customer.Find(nOrder.CustomerId);
+                orderData.Menu = DineOutContext.Menu.Find(order.Item.MenuId);
                 orderData.Items = DineOutContext.Item
                     .ToList().FindAll(x => x.MenuId == order.Item.MenuId);
                 orderData.Restaurant = DineOutContext.Restaurant
                     .Find(orderData.Menu.RestaurantId);
-                foreach (Item item in orderData.Items)
-                {
-                    orderData.Item = DineOutContext.Item
-                        .Find(item.ItemId);
-                }
 
                 TempData["message"] = "Item is added";
-                return View(orderData);
+                return View("OrderSummary", orderData);
             }
             else
             {
+                orderData.Order
+               = DineOutContext.Order.Find(nOrder.OrderId);
+                orderData.Customer
+                   = DineOutContext.Customer.Find(nOrder.CustomerId);
+                orderData.Menu = DineOutContext.Menu.Find(orderData.Menu.RestaurantId);
+                orderData.Items = DineOutContext.Item
+                    .ToList().FindAll(x => x.MenuId == orderData.Menu.RestaurantId);
+                orderData.Restaurant = DineOutContext.Restaurant
+                    .Find(orderData.Menu.RestaurantId);
+                foreach (Item item in orderData.Items)
+                {
+                    orderData.Item = DineOutContext.Item.Find(item.ItemId);
+                }
+
                 TempData["message"] = "Sorry, there is an error. Plese try it again.";
-                return View(order.Item.MenuId);
+                return View("OrderDetails", orderData);
             }
-        }
-
-        //Add to CustomerOrder Model
-        public IActionResult AddToCustomerOrder(int itemId)
-        {
-            Item item = DineOutContext.Item
-                .FirstOrDefault(p => p.ItemId == itemId);
-
-            if (item != null)
-            {
-                CustomerOrder customerOrder = GetCustomerOrder();
-                customerOrder.AddItem(item, 1);
-                SaveCustomerOrder(customerOrder);
-            }
-
-            //How do I return a view with reflecting current order status (quentity)?
-            return View("OrderSummary");
-
-        }
-
-        public IActionResult RemoveFromCustomerOrder(int itemId)
-        {
-            Item item = DineOutContext.Item
-                .FirstOrDefault(p => p.ItemId == itemId);
-
-            if (item != null)
-            {
-                CustomerOrder customerOrder = GetCustomerOrder();
-                customerOrder.RemoveLine(item);
-                SaveCustomerOrder(customerOrder);
-            }
-
-            //How do I return a view with reflecting current order status (quentity)?
-            return View("OrderSummary");
-        }
-
-        private void SaveCustomerOrder(CustomerOrder customerOrder)
-        {
-            HttpContext.Session.SetJson("CustomerOrder", customerOrder);
-
-        }
-
-        private CustomerOrder GetCustomerOrder()
-        {
-            CustomerOrder customerOrder =
-                HttpContext.Session.GetJson<CustomerOrder>("CustomerOrder")
-                ?? new CustomerOrder();
-
-            return customerOrder;
         }
 
         //Display order summary
         public IActionResult OrderSummary(int orderId)
         {
-            //For testing purpose
-            orderId = 2;
             List<Item> items = new List<Item>();
-            CustomerOrderViewModel orderData = new CustomerOrderViewModel();
+            //CustomerOrderViewModel orderData = new CustomerOrderViewModel();
+            Order nOrder = new Order
+            {
+                OrderId = orderId
+            };
+            DineOutContext.Order.Update(nOrder);
 
-            orderData.Order = DineOutContext.Order.Find(orderId);
+            orderData.Order = DineOutContext.Order.Find(nOrder.OrderId);
             orderData.OrderItems = DineOutContext.Order_Item
                 .ToList().FindAll(x => x.OrderId == orderId);
+
+            orderData.OrderItem = DineOutContext.Order_Item.Find(nOrder.OrderId);
+            //OrderItem is not found and added even though it can be found in above
             orderData.Restaurant = DineOutContext.Restaurant
-                    .Find(orderData.Order.RestaurantId);
+                .Find(orderData.Order.RestaurantId);
             foreach (OrderItem orderItem in orderData.OrderItems)
             {
-                orderItem.Item = DineOutContext.Item
-                    .Find(orderItem.ItemId);
+                orderItem.Item = DineOutContext.Item.Find(orderItem.ItemId);
             }
 
             return View(orderData);
-            //return View(DineOutContext.Order.Where(p => p.OrderId == orderId));
-            /*return View(new CustomerOrderViewModel
-            {
-                CustomerOrder = GetCustomerOrder()
-            });*/
         }
 
 
