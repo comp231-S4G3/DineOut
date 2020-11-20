12ab2351aabcd77a40af1dd8b8444c1759252c63
younghwiKim <dudgnl37@gmail.com>
2020-11-20 14:40:08 +0900

    added Customer Profile View

---
+++ DineOut/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DineOut.Models;
+
+namespace DineOut.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CustomerLogin() => View();
+        public IActionResult CustomerRegistration() => View();
+
+        [HttpPost]
+        public IActionResult CustomerLogin(Customer customer)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(Customer customer)
+        {
+            return View();
+        }
+
+    }
+}
