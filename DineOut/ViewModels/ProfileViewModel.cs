b11578c664cd44a26cdc78be6d6e124f9babd946
Dioums <mdiallo8@my.centennialcollege.ca>
2020-11-22 22:27:43 -0500

    profile view created and related action, update profile functionality implemented, and small view enhancements

---
+++ DineOut/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DineOut.Models;
+
+namespace DineOut.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public RestaurantProfile restaurantProfile { get; set; }
+
+        public Restaurant restaurant { get; set; }
+    }
+}
